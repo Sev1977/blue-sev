2ecbac38fe26c207be0553946aeb31fc90fa1657
andrew.burns <andrew.burns@cellnovo.com>
2020-10-29 18:34:14 +0000

    Added a filter to the competitions list.

--- app/src/main/java/android/personal/fixtures/fragments/CompetitionsFragment.java
+++ app/src/main/java/android/personal/fixtures/fragments/CompetitionsFragment.java
@@ -1,6 +1,7 @@
 package android.personal.fixtures.fragments;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.personal.fixtures.R;
@@ -8,11 +9,13 @@ import android.personal.fixtures.adapters.CompetitionRecyclerViewAdapter;
 import android.personal.fixtures.database.Database;
 import android.personal.fixtures.database.tables.Competitions;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * A fragment representing a list of Competitions.
@@ -26,10 +29,12 @@ public class CompetitionsFragment extends Fragment
 
     private Cursor mCompetitions;
     private OnCompetitionsListInteractionListener mListener;
+    private Database mDatabase;
+    private RecyclerView mList;
 
     /**
-     * Mandatory empty constructor for the fragment manager to instantiate the
-     * fragment (e.g. upon screen orientation changes).
+     * Mandatory empty constructor for the fragment manager to instantiate the fragment (e.g. upon
+     * screen orientation changes).
      */
     public CompetitionsFragment()
     {
@@ -40,8 +45,10 @@ public class CompetitionsFragment extends Fragment
     {
         super.onCreate(savedInstanceState);
 
-        mCompetitions = Database.getInstance(getActivity().getApplicationContext()).getAllRecords(
-                Competitions.TABLE_NAME, Competitions.DEFAULT_SORT_ORDER);
+        mDatabase = Database.getInstance(getActivity());
+
+        mCompetitions = mDatabase.getAllRecords(Competitions.TABLE_NAME,
+                Competitions.DEFAULT_SORT_ORDER);
     }
 
     @Override
@@ -51,17 +58,65 @@ public class CompetitionsFragment extends Fragment
         View view = inflater.inflate(R.layout.fragment_competition_list, container, false);
 
         // Set the adapter
-        if (view instanceof RecyclerView)
+        final View list = view.findViewById(R.id.competitions_recycler_view);
+        if (list instanceof RecyclerView)
         {
-            Context context = view.getContext();
-            RecyclerView recyclerView = (RecyclerView)view;
-            recyclerView.setLayoutManager(new LinearLayoutManager(context));
-            recyclerView.setAdapter(new CompetitionRecyclerViewAdapter(mCompetitions, mListener));
+            mList = (RecyclerView)list;
+            mList.setLayoutManager(new LinearLayoutManager(view.getContext()));
+            mList.setAdapter(new CompetitionRecyclerViewAdapter(mCompetitions, mListener));
         }
+
+        final Context context = view.getContext();
+        final String[] filterItems = new String[]{context.getString(R.string.comps_filter_all),
+                context.getString(R.string.comps_filter_leagues), context.getString(
+                R.string.comps_filter_cups)};
+
+        view.findViewById(R.id.competitions_list_filter).setOnClickListener(
+                new View.OnClickListener()
+                {
+                    @Override
+                    public void onClick(final View view)
+                    {
+                        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(context);
+                        dialogBuilder.setTitle("Filter").setNegativeButton(android.R.string.cancel,
+                                null).setItems(filterItems, new DialogInterface.OnClickListener()
+                        {
+                            @Override
+                            public void onClick(final DialogInterface dialogInterface, final int i)
+                            {
+                                ((TextView)view.findViewById(R.id.competitions_filter_label))
+                                        .setText(filterItems[i]);
+                                switch (i)
+                                {
+                                    case 0: // All
+                                        mCompetitions = mDatabase.getAllRecords(
+                                                Competitions.TABLE_NAME,
+                                                Competitions.DEFAULT_SORT_ORDER);
+                                        break;
+
+                                    case 1: // Leagues
+                                        mCompetitions = mDatabase.getColumnsForSelection(
+                                                Competitions.TABLE_NAME, null,
+                                                Competitions.COL_NAME_IS_LEAGUE + "=1", null,
+                                                Competitions.DEFAULT_SORT_ORDER);
+                                        break;
+
+                                    case 2:
+                                        mCompetitions = mDatabase.getColumnsForSelection(
+                                                Competitions.TABLE_NAME, null,
+                                                Competitions.COL_NAME_IS_LEAGUE + "=0", null,
+                                                Competitions.DEFAULT_SORT_ORDER);
+                                        break;
+                                }
+                                mList.setAdapter(new CompetitionRecyclerViewAdapter(mCompetitions,
+                                        mListener));
+                            }
+                        }).create().show();
+                    }
+                });
 
         return view;
     }
-
 
     @Override
     public void onAttach(Context context)
@@ -92,7 +147,7 @@ public class CompetitionsFragment extends Fragment
      * <p/>
      * See the Android Training lesson
      * <a href="http://developer.android.com/training/basics/fragments/communicating.html">
-     *     Communicating with Other Fragments</a> for more information.
+     * Communicating with Other Fragments</a> for more information.
      */
     public interface OnCompetitionsListInteractionListener
     {
